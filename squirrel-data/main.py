9ea94326f70e9c4a07ef9dc99c278c0e6b59bf86
Timmy <timmyo@live.se>
2021-12-29 14:17:46 +0100

    Read from file and print colors to new csv file

--- squirrel-data/main.py
+++ squirrel-data/main.py
@@ -12,6 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# how many of each color fur is there
+# how many of each color fur is there ( Primary Fur Color )
 #New csv with this data.
-#Primary Fur Color
+
+import pandas
+
+data = pandas.read_csv("./squirrel_data.csv")
+
+# black = 0
+# cinnamon = 0
+# grey = 0
+
+# fur_colors = data["Primary Fur Color"]
+
+# for color in fur_colors:
+#     if color == "Black":
+#         black += 1
+#     elif color == "Grey":
+#         grey += 1
+#     elif color == "Cinnamon":
+#         cinnamon += 1
+
+# print(f"Black: {black}")
+# print(f"Cinnamon: {cinnamon}")
+# print(f"Grey {grey}")
